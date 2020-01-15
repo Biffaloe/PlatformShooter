b13670fe61a981bca7948aee94e5479102677cf5
KEVIN.ROSS9569@OMAIL.OTECH.EDU <4400089569@OWATC.TECH>
2020-01-15 10:36:54 -0700

    Added Assets

---
+++ PlatformShooter/Assets/Scripts/CameraControllerPlatfroming.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControllerPlatfroming : MonoBehaviour {
+
+    public Transform target;
+    public Vector3 offset;
+    public bool InvertY;
+    public bool usedOffsetValues;
+    public float MaxViewAngle;
+    public float MinViewAngle;
+    public float rotateSpeed;
+    public Transform pivot;
+
+	void Start ()
+    {
+        if ( !usedOffsetValues)
+            offset = target.position - transform.position;
+
+        pivot.transform.position = target.transform.position;
+        pivot.transform.parent = target.transform;
+
+        Cursor.lockState = CursorLockMode.Locked;
+	}
+	
+	void LateUpdate ()
+    {
+        //get x and y pos of the mouse and rotate the target
+        float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
+        target.Rotate(0, horizontal, 0);
+        float vertical = Input.GetAxis("Mouse Y") * rotateSpeed;
+        if (InvertY)
+            pivot.Rotate(vertical, 0, 0);
+        else
+            pivot.Rotate(-vertical, 0, 0);
+
+
+        //limit up/down camera rotation
+        if (pivot.rotation.eulerAngles.x > MaxViewAngle && pivot.rotation.eulerAngles.x < 180f)
+            pivot.rotation = Quaternion.Euler(MaxViewAngle, 0, 0);
+        if (pivot.rotation.eulerAngles.x > 180f && pivot.rotation.eulerAngles.x < (360 + MinViewAngle))
+            pivot.rotation = Quaternion.Euler((360f + MinViewAngle),0,0);
+
+
+
+        //Move the camera based on the current rotation of the target;
+        float desiredYAngle = target.eulerAngles.y;
+        float desiredXAngle = pivot.eulerAngles.x;
+
+        Quaternion rotation = Quaternion.Euler(desiredXAngle, desiredYAngle, 0);
+        transform.position = target.position - (rotation * offset);
+
+        if (transform.position.y < target.position.y)
+        {
+            transform.position = new Vector3(transform.position.x, target.position.y, transform.position.z);
+        }
+
+        //transform.position = target.position - offset;
+        transform.LookAt(target);
+	}
+}
---
+++ PlatformShooter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController: MonoBehaviour
+{
+    bool isGrounded = true;
+    public Rigidbody RB;
+    private Vector3 movement;
+    private bool canJump;
+    public float Speed; 
+    public float jumpForce;
+    public float GravityScale;
+    public new Camera camera;
+
+
+	void Start ()
+    {
+        RB = GetComponent<Rigidbody>();
+	}
+    private void Update()
+    {
+        movement = new Vector3(Input.GetAxis("Horizontal"), movement.y, Input.GetAxis("Vertical"));
+        if(isGrounded == true)
+        {
+            if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+            {
+                RB.velocity = new Vector3(movement.x, jumpForce, movement.z);
+            }
+        }
+    }
+    private void FixedUpdate()
+    {
+        moveCharacter(movement);
+    }
+    void moveCharacter(Vector3 direction)
+    {
+        //reading the input:
+        float horizontalAxis = Input.GetAxis("Horizontal");
+        float verticalAxis = Input.GetAxis("Vertical");
+
+        //camera forward and right vectors:
+        var forward = camera.transform.forward;
+        var right = camera.transform.right;
+
+        //project forward and right vectors on the horizontal plane (y = 0)
+        forward.y = 0f;
+        right.y = 0f;
+        forward.Normalize();
+        right.Normalize();
+
+        //this is the direction in the world space we want to move:
+        var desiredMoveDirection = forward * verticalAxis + right * horizontalAxis;
+
+        //now we can apply the movement:
+        RB.MovePosition((Vector3)transform.position + (desiredMoveDirection * Speed * Time.deltaTime));
+    }
+}
